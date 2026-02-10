3c37077ba4488a37f91ac2e39dccc0fa890987c0
mimmo <MIMMOSEMERARO99@gmail.com>
2026-02-11 00:33:50 +0100

    Add files via upload

---
+++ classsfxmanager.js
@@ -0,0 +1,9 @@
+var classsfxmanager =
+[
+    [ "Awake", "classsfxmanager.html#a10c5755ab0d767dd629a951a1e6b066f", null ],
+    [ "PlaySFX", "classsfxmanager.html#af50259142fe2785706950fb8bc1d6dc9", null ],
+    [ "PlaySFX", "classsfxmanager.html#af0ed3c5e51f3735fa4566caef1ba1f30", null ],
+    [ "audioSource", "classsfxmanager.html#a051a21d27d1db43f91c370c04574c1c2", null ],
+    [ "pitchVariation", "classsfxmanager.html#ab9d080a5aefe41f60257ee78f86059a3", null ],
+    [ "soundEffect", "classsfxmanager.html#a0ecaf5b34238d130b827101ef9363f87", null ]
+];
---
+++ classsuggerimenti.js
@@ -0,0 +1,9 @@
+var classsuggerimenti =
+[
+    [ "Mostra_indizio", "classsuggerimenti.html#a0459a0ab837d0db14caa10d47a868c89", null ],
+    [ "OnEnable", "classsuggerimenti.html#ac5d27d14912c5ee759812a324a13387d", null ],
+    [ "Start", "classsuggerimenti.html#abbd4ded076c5d318c0b01d166c71e341", null ],
+    [ "Update", "classsuggerimenti.html#a855c26e533bf5cb37091d3602a35d84b", null ],
+    [ "ListaIndizi", "classsuggerimenti.html#aae170b443e3ccc3dcf51cc46365a9737", null ],
+    [ "TestoDaMostrare", "classsuggerimenti.html#a76b25e364fe6a991c80a64ce06f01b47", null ]
+];
